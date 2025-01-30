f5f651931bf5f19ca462ce80d8208458e1dd74af
Michael Adenrele <michaeladenrele18@gmail.com>
2025-01-30 09:35:13 -0600

    Replace the hello-world program with the sample C program.

--- hello-world.c
+++ hello-world.c
@@ -1,4 +1,32 @@
 #include <stdio.h>
-int main(){
-    printf("bye world");
+#include <string.h>
+
+int main() {
+    char str[100];
+    int i, len;
+
+    printf("Enter a string: ");
+    gets(str);  // Warning: gets() is unsafe; use fgets() instead in modern C
+
+    len = strlen(str);
+
+    // Convert to uppercase
+    for (i = 0; i < len; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - 'a' + 'A';
+        }
+    }
+
+    printf("The capitalized string is: %s\n", str);
+
+    // Reverse the string
+    for (i = 0, len = strlen(str); i < len / 2; i++) {
+        char temp = str[i];
+        str[i] = str[len - 1 - i];
+        str[len - 1 - i] = temp;
+    }
+
+    printf("The reversed string is: %s\n", str);
+
+    return 0;
 }
